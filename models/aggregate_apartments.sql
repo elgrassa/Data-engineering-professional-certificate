1de318753b1d9ac53e4b471842ea19343b5e2b2b
pavloskorodziievskyi <pavlo.skorodziievskyi@gmail.com>
2025-04-28 18:21:33 +0200

    updated aggregate

--- models/aggregate_apartments.sql
+++ models/aggregate_apartments.sql
@@ -5,5 +5,16 @@ SELECT
     AVG(price) AS average_price,
     MIN(price) AS min_price,
     MAX(price) AS max_price
-FROM {{ ref('apartments_sale') }}  -- Refers to the apartments_sale source model
+FROM {{ source('polish_flats', 'apartments_sale') }}  -- Refers to the apartments_sale source model
+GROUP BY city
+
+UNION ALL
+
+SELECT
+    city,
+    COUNT(id) AS total_listings,
+    AVG(price) AS average_price,
+    MIN(price) AS min_price,
+    MAX(price) AS max_price
+FROM {{ source('polish_flats', 'apartments_rent') }}  -- Refers to the apartments_rent source model
 GROUP BY city
